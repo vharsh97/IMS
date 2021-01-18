fb09658f0888034bc4188ce86115ed34ea9ff692
vharsh97 <vharsh.srivastava@gmail.com>
2021-01-18 18:01:25 +0530

    Users initial changes

--- Users.Designer.cs
+++ Users.Designer.cs
@@ -29,15 +29,133 @@ namespace IMS
         /// </summary>
         private void InitializeComponent()
         {
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.leftPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // leftPanel
             // 
+            this.leftPanel.Controls.Add(this.textBox5);
+            this.leftPanel.Controls.Add(this.textBox4);
+            this.leftPanel.Controls.Add(this.label7);
+            this.leftPanel.Controls.Add(this.label6);
+            this.leftPanel.Controls.Add(this.textBox3);
+            this.leftPanel.Controls.Add(this.label5);
+            this.leftPanel.Controls.Add(this.textBox2);
+            this.leftPanel.Controls.Add(this.label4);
+            this.leftPanel.Controls.Add(this.textBox1);
+            this.leftPanel.Controls.Add(this.label3);
             this.leftPanel.Size = new System.Drawing.Size(250, 581);
+            this.leftPanel.TabIndex = 1;
+            this.leftPanel.Controls.SetChildIndex(this.label3, 0);
+            this.leftPanel.Controls.SetChildIndex(this.textBox1, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label4, 0);
+            this.leftPanel.Controls.SetChildIndex(this.textBox2, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label5, 0);
+            this.leftPanel.Controls.SetChildIndex(this.textBox3, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label6, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label7, 0);
+            this.leftPanel.Controls.SetChildIndex(this.textBox4, 0);
+            this.leftPanel.Controls.SetChildIndex(this.textBox5, 0);
             // 
             // rightPanel
             // 
             this.rightPanel.Size = new System.Drawing.Size(799, 581);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 192);
+            this.textBox1.MaxLength = 35;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 27);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 245);
+            this.textBox2.MaxLength = 20;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(225, 27);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Usename";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 298);
+            this.textBox3.MaxLength = 20;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(225, 27);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Password";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 351);
+            this.textBox4.MaxLength = 10;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(225, 27);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 328);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(194, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Phone (+91 - XXXXXXXXXX)";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 404);
+            this.textBox5.MaxLength = 50;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(225, 27);
+            this.textBox5.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 381);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Email";
             // 
             // Users
             // 
@@ -46,10 +164,23 @@ namespace IMS
             this.ClientSize = new System.Drawing.Size(1049, 581);
             this.Name = "Users";
             this.Text = "Users";
+            this.leftPanel.ResumeLayout(false);
+            this.leftPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
